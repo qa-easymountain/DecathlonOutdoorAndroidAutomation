9fae882e45ab252fff25683e602dcefd4c0364d0
Christophe TARET <ch.taret@gmail.com>
2022-05-18 11:48:39 +0200

    refactor: move initialization code of android driver to an utility class

--- src/test/java/com/decathlon/outdoor/ExampleTest.java
+++ src/test/java/com/decathlon/outdoor/ExampleTest.java
@@ -3,18 +3,14 @@ package com.decathlon.outdoor;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.remote.MobileCapabilityType;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URL;
 
 class ExampleTest {
 
@@ -22,20 +18,8 @@ class ExampleTest {
 
     @BeforeEach
     void setup() throws MalformedURLException {
-        File app = new File("apk/4.16.0 adding_missing_ids.apk");
-        DesiredCapabilities cap = new DesiredCapabilities();
-        //here we set our android emulator
-        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "PoojaEmulatorPixel5API32");
-        //here we set our capability type
-        cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
-
-        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
-
-        cap.setCapability("appPackage", "com.decathlon.quechuafinder");
-        cap.setCapability("platform", "Android");
-        cap.setCapability("appActivity", "com.easymountain.quechua.ui.main.MainActivity");
-        //cap.setCapability("noReset", "true");
-        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+        var apkFile = "4.16.0 adding_missing_ids.apk";
+        driver = AndroidDriverBuilder.buildDriver(apkFile);
     }
 
     @Test
