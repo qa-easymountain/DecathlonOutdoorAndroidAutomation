35a5ad7d67152283a8bf78424090381a26919cb2
Pooja-Sangani <pooja@easy-mountain.com>
2022-05-28 17:31:23 +0200

    add Junit 5 for Test and TestSuites

---
+++ src/test/java/com/decathlon/outdoor/TestSuite.java
@@ -0,0 +1,9 @@
+package com.decathlon.outdoor;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite()
+@SelectClasses({Login.class})
+public class TestSuite {
+}  	
