d0a6c08db14005f9a505911f2265baa396a6c22a
Pooja-Sangani <pooja@easy-mountain.com>
2022-06-09 07:27:15 +0200

    fix: code refactor

--- src/test/java/com/decathlon/outdoor/DecathlonOutdoorTestSuite.java
+++ src/test/java/com/decathlon/outdoor/DecathlonOutdoorTestSuite.java
@@ -1,6 +1,5 @@
 package com.decathlon.outdoor;
 
-import org.junit.platform.suite.api.IncludePackages;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
