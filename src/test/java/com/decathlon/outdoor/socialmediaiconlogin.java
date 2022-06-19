16ab835043e4a2ebd0441475902d8e23cf424e88
Pooja-Sangani <pooja@easy-mountain.com>
2022-06-19 19:32:09 +0200

    Feat:Social media login

---
+++ src/test/java/com/decathlon/outdoor/socialmediaiconlogin.java
@@ -0,0 +1,139 @@
+package com.decathlon.outdoor;
+
+        import io.appium.java_client.MobileBy;
+        import io.appium.java_client.android.AndroidDriver;
+        import io.appium.java_client.android.AndroidElement;
+        import org.assertj.core.api.Assertions;
+        import org.junit.jupiter.api.*;
+        import org.openqa.selenium.NoSuchElementException;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.support.ui.ExpectedConditions;
+        import org.openqa.selenium.support.ui.WebDriverWait;
+        import java.net.MalformedURLException;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class socialmediaiconlogin {
+    private AndroidDriver<AndroidElement> driver;
+
+    @BeforeEach
+    public void setup() throws MalformedURLException {
+        driver = AndroidDriverBuilder.buildDriver();
+    }
+
+    @Test
+    @DisplayName("it should be able to login")
+    public void userLogin() throws InterruptedException {
+        String packageName = driver.getCurrentPackage();
+
+        AndroidElement acceptAndCloseButton = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(MobileBy.id(packageName + ":id/button_agree")));
+        acceptAndCloseButton.click();
+        System.out.println("Cliqué sur Accepter & Fermer");
+
+        //Onboard and procees Button
+        AndroidElement welcomeAgree = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(MobileBy.id(packageName +":id/onboard_proceed_btn")));
+        welcomeAgree.click();
+
+        // select location next to you
+        AndroidElement locationSelector = (AndroidElement) new WebDriverWait(driver,  30).until(
+                //ExpectedConditions.elementToBeClickable(MobileBy.id("com.android.permissioncontroller:id/permission_location_accuracy_radio_fine")));
+                ExpectedConditions.presenceOfElementLocated(MobileBy.xpath("//android.widget.RadioButton[@resource-id='com.android.permissioncontroller:id/permission_location_accuracy_radio_fine']")));
+
+        locationSelector.click();
+
+        // give permission while using application
+        AndroidElement locationPermissionAllowUsingThisAppButton = (AndroidElement) new WebDriverWait(driver, 30).until(
+                //ExpectedConditions.presenceOfElementLocated(MobileBy.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button"))
+                ExpectedConditions.presenceOfElementLocated(MobileBy.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button"))
+        );
+        locationPermissionAllowUsingThisAppButton.click();
+
+        // GoTo Profile Page for Login (click on profile option)
+        //Thread.sleep(4000);
+        AndroidElement profilePageButton = (AndroidElement) new WebDriverWait(driver, 100).until(
+                //ExpectedConditions.presenceOfElementLocated(MobileBy.id(packageName + ":id/profile_nav_menu"))
+                ExpectedConditions.presenceOfElementLocated(MobileBy.xpath("//android.widget.FrameLayout[@content-desc='Profil']"))
+        );
+        profilePageButton.click();
+
+        //click on connect to login page //Continue avec decathlon
+        //Thread.sleep(3000);
+        AndroidElement decathlonConnectionButton = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.presenceOfElementLocated(MobileBy.xpath("//android.view.ViewGroup[@resource-id='" + packageName + ":id/btn_decathlon_login']"))
+        );
+        decathlonConnectionButton.click();
+
+        //Click on facebook icon
+
+        //Thread.sleep(30000);
+        AndroidElement userNameInput = (AndroidElement) new WebDriverWait(driver, 50).until(
+                //Formula of xpath=> .xpath("//Class name[@attribute name='value']")
+                ExpectedConditions.presenceOfElementLocated(MobileBy.xpath("//android.widget.Button[@resource-id='color-facebook']"))
+        );
+        userNameInput.click();
+
+
+        //click on allow essential and optional cookies
+        //Thread.sleep(3000);
+        AndroidElement essentialcookies = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.presenceOfElementLocated(MobileBy.xpath("//android.widget.Button[@resource-id='u_0_m_JW']"))
+        );
+        essentialcookies.click();
+
+        //Enter facebook email address
+        AndroidElement userNameInput = (AndroidElement) new WebDriverWait(driver, 50).until(
+                //Formula of xpath=> .xpath("//Class name[@attribute name='value']")
+                ExpectedConditions.presenceOfElementLocated(MobileBy.xpath("//android.widget.EditText[@resource-id='m_login_email']"))
+        );
+        userNameInput.click();
+        userNameInput.sendKeys("android.mhikes@gmail.com");
+
+        // insert password
+        //Thread.sleep(3000);
+        AndroidElement passwordInput = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.presenceOfElementLocated(MobileBy.xpath("//android.widget.EditText[@resource-id='m_login_password']"))
+        );
+        passwordInput.sendKeys("SXX8fFMbA4aqb");
+
+        //click on Log in button
+        //Thread.sleep(3000);
+        AndroidElement loginButton = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.presenceOfElementLocated(MobileBy.xpath("//android.widget.Button[@text='Log in']"))
+        );
+
+        loginButton.click();
+
+        //Click on gothrough first hike
+        Thread.sleep(3000);
+        AndroidElement usernameProfile = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.presenceOfElementLocated(MobileBy.id(packageName + ":id/username_profile"))//text-Trouver ma première balade and class- android.widget.Button
+        );
+        Assertions.assertThat(usernameProfile.isDisplayed()).isEqualTo(true);
+        System.out.println("Login Successfully");
+
+        Thread.sleep(5000);
+        AndroidElement settingsButton = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.presenceOfElementLocated(MobileBy.id(packageName + ":id/settings"))
+        );
+        settingsButton.click();
+
+        Thread.sleep(3000);
+        AndroidElement logoutButton = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.presenceOfElementLocated(MobileBy.id(packageName + ":id/disconnect_btn"))
+        );
+        logoutButton.click();
+        System.out.println("Logout after Successful login");
+
+        Thread.sleep(5000);
+
+    }
+
+    @AfterAll()
+    public void tearDown() {
+        if(null != driver) {
+            System.out.println("quiting the driver");
+            driver.quit();
+        }
+    }
+}
