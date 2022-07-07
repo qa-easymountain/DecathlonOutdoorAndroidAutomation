61abf3a29892c93ca0baaaa4e4f323890fe90e72
Pooja-Sangani <pooja@easy-mountain.com>
2022-07-07 09:59:52 +0200

    Updating contribution feature

--- src/test/java/com/decathlon/outdoor/Contribution.java
+++ src/test/java/com/decathlon/outdoor/Contribution.java
@@ -17,8 +17,8 @@ public class Contribution {
     @BeforeEach
     public void setup() throws IOException {
         //   driver = BasicSauceLabsConfiguration.setup();
-        //driver = BrowserstackBuilder.buildDriver("Contribution");
-        driver = AndroidDriverBuilder.buildDriver();
+        driver = BrowserstackBuilder.buildDriver("Contribution");
+        //driver = AndroidDriverBuilder.buildDriver();
     }
 
     @Test
